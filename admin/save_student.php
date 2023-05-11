5d4cd465a44cdbebf88db1e5b85c7de9d6d3e086
nallalisa <blackwheldeguia@gmail.com>
2023-05-12 04:55:08 +0800

    boss mike

--- admin/save_student.php
+++ admin/save_student.php
@@ -1,12 +1,19 @@
 <?php
 include('dbcon.php');
-        
-               $un = $_POST['un'];
-               $fn = $_POST['fn'];
-               $ln = $_POST['ln'];
-               $class_id = $_POST['class_id'];
 
-               mysqli_query($conn,"insert into student (username,firstname,lastname,location,class_id,status)
+$un = $_POST['un'];
+$fn = $_POST['fn'];
+$ln = $_POST['ln'];
+$class_id = $_POST['class_id'];
+
+$sql = "SELECT * FROM student WHERE username = '$un'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    echo 'error_username';
+} else {
+    mysqli_query($conn, "insert into student (username,firstname,lastname,location,class_id,status)
 		values ('$un','$fn','$ln','uploads/NO-IMAGE-AVAILABLE.jpg','$class_id','Unregistered')                                    
-		") or die(mysqli_error()); ?>
-<?php    ?>
+		") or die(mysqli_error());
+    echo 'success';
+}
+?>
